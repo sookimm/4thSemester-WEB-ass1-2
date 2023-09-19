06326c25692860c564d3feb34202d359638a7624
Sooyeon Kim <105490589+sookimm@users.noreply.github.com>
2023-09-19 01:00:54 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
-/*********************************************************************************
- *  WEB422 – Assignment 2
- *  I declare that this assignment is my own work in accordance with Seneca Academic Policy.
- *  No part of this assignment has been copied manually or electronically from any other source
- *  (including web sites) or distributed to other students.
- *
- *  Name: Sooyeon Kim     Student ID: 149385213      Date: June 3, 2023
- *
- ********************************************************************************/
-
 let page = 1; // to store the current page number
 const perPage = 10; // number of movies per pages
 
